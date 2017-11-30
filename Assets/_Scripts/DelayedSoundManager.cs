585740bc7adefb6b8a7882f8ddad6e777fadab2e
dr-matt-smith <dr_matt_smith@me.com>
2017-11-30 08:08:52 +0000

    added files to project

---
+++ Assets/_Scripts/DelayedSoundManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DelayedSoundManager : MonoBehaviour 
+{
+    public AudioSource audioSourcePacmandMusic;
+    public AudioSource audioSourceDies;
+
+    /// <summary>
+    /// play the pacman music immediately
+    /// </summary>
+    public void ACTION_PlayMusicNow()
+    {
+        audioSourcePacmandMusic.Play();
+    }
+
+    /// <summary>
+    /// play the pacman dies sound after a 1.0 second delay
+    /// </summary>
+    public void ACTION_PlayDiesSoundAfterDelay()
+    {
+        float delay = 1.0F;
+        audioSourceDies.PlayDelayed(delay);
+    }
+}
